69ca002f67d4a24551e1eb255996d8c020cb196e
dimaatiye <c00301176@setu.ie>
2026-09-24 14:39:39 +0100

    adding Wander Enemy behav

--- LAB1/LAB1/WanderNPC.cpp
+++ LAB1/LAB1/WanderNPC.cpp
@@ -1,14 +1,80 @@
 #include "WanderNPC.h"
+#include <cmath>
 
 WanderNPC::WanderNPC(sf::Texture& WanderTexture) :WanderSprite(WanderTexture)
 {
 	WanderSprite.setScale({ 0.15,0.15 });
-	WanderSprite.setPosition({ 400, 400 });
-	orientationW = 0.0f;
-	wanderOrientation = 0.0f;
-	wanderRate = 0.1f;
-	wanderOffset = 100.0f;
-	wanderRadius = 50.0f;
+	bounds = WanderSprite.getLocalBounds();
+	WanderSprite.setOrigin({ bounds.size.x / 2.f, bounds.size.y / 2.f }); // rotate around the centre
+
+	WanderSprite.setPosition({ 100, 400 });
+}
+void WanderNPC::updateW()
+{
+	float dt = clockW.restart().asSeconds();
+
+	//lecture: wanderOrientation += random(-1,+1) * wanderRate
+	float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 2.0f - 1.0f;
+	wanderOrientation += r * wanderRate;
+
+	//lecture: targetOrientation = wanderOrientation + orientation
+	float targetOrientation = wanderOrientation + orientationW;
+
+	// lecture: target = my.position + wanderOffset * my.orientation.asVector()
+	sf::Vector2f facing(std::cos(orientationW), std::sin(orientationW));
+	sf::Vector2f circleCentre = WanderSprite.getPosition() + wanderOffset * facing;
+
+	sf::Vector2f targetFacing(std::cos(targetOrientation), std::sin(targetOrientation));
+	sf::Vector2f target = circleCentre + wanderRadius * targetFacing;
+
+	sf::Vector2f toTarget = target - WanderSprite.getPosition();
+	float desiredOrientation = std::atan2(toTarget.y, toTarget.x);
+
+	float rotationDiff = std::atan2(std::sin(desiredOrientation - orientationW),std::cos(desiredOrientation - orientationW));
+
+	float maxRotation = 1.5f * dt; 
+	if (rotationDiff > maxRotation)
+	{
+		rotationDiff = maxRotation;
+	}
+	if (rotationDiff < -maxRotation) 
+	{
+		rotationDiff = -maxRotation;
+	}
+
+
+	orientationW += rotationDiff;
+
+	//acceleration in the direction sprite now facing
+	sf::Vector2f acceleration = maxAcceleration * sf::Vector2f(std::cos(orientationW), std::sin(orientationW));
+
+	
+	float damping = 3.0f; 
+	velocityW += acceleration * dt;
+	velocityW -= velocityW * damping * dt; 
+	float speed = std::sqrt(velocityW.x * velocityW.x + velocityW.y * velocityW.y);
+	if (speed > maxSpeed)
+	{
+		velocityW *= (maxSpeed / speed);
+	}
+
+	WanderSprite.move(velocityW * dt);
+	WanderSprite.setRotation(sf::radians(orientationW) + sf::degrees(90.f)); 
+	sf::FloatRect currentBounds = WanderSprite.getGlobalBounds();
+
+	if (currentBounds.position.x < -1 ||
+		currentBounds.position.x + currentBounds.size.x > 900)
+	{
+		velocityW.x = -velocityW.x;
+
+	}
+
+	if (currentBounds.position.y < -1 ||
+		currentBounds.position.y + currentBounds.size.y > 900)
+	{
+		velocityW.y = -velocityW.y;
+
+	}
 }
 void WanderNPC::draw(sf::RenderWindow& window)
 {
--- LAB1/LAB1/WanderNPC.h
+++ LAB1/LAB1/WanderNPC.h
@@ -3,15 +3,19 @@
 class WanderNPC
 {private :
 	sf::Sprite WanderSprite;
-	float orientationW;
-	float wanderOrientation;
-	float wanderRate;
-	float maxAcceleration;
-	float wanderRadius;
-	float wanderOffset;
+	float orientationW = 0.0f;;
+	float wanderOrientation = 0.0f;;
+	float wanderRate = 0.1f;;
+	float maxAcceleration = 200.0f;
+	float wanderRadius = 50.0f;
+	float wanderOffset = 100.0f;
+	sf::Vector2f velocityW{ 0.0f, 0.0f };
+	float maxSpeed = 200.0f;
+	sf::Clock clockW;
+	sf::FloatRect bounds;
 public:
 	WanderNPC(sf::Texture& WanderTexture);
-	void update();
+	void updateW();
 	void draw(sf::RenderWindow& window);
 };
 
